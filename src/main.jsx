92c10ce402b221e440a434c635a61863a113706a
Davison Campos <166734630+DavisonCDev@users.noreply.github.com>
2026-02-06 17:34:02 -0400

    Migração para React
    Acerto do grid da galeria
    Modularização do components

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
 // IMPORTANDO OS ESTILOS
-// Os nomes dos arquivos batem com o que está na pasta src/styles
 import './styles/variables.css'
 import './styles/extras.css'
 import './styles/navbar.css'
 import './styles/hero.css'
-import './styles/components.css'
+import './styles/marquee.css'
+import './styles/music.css'
+import './styles/gallery.css'
 import './styles/tour.css'
 import './styles/store.css'
 import './styles/footer.css'
